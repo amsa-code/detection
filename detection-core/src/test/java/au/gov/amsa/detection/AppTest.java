f9f36fb772159a9c2a1afa8f7daf4d6055c71bba
Dave Moten <davidmoten@gmail.com>
2015-08-19 17:01:11 +1000

    dev

--- detection-core/src/test/java/au/gov/amsa/detection/AppTest.java
+++ detection-core/src/test/java/au/gov/amsa/detection/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import au.gov.amsa.detection.model.CraftType;
 import xuml.tools.util.database.DerbyUtil;
 
 public class AppTest {
@@ -18,6 +19,7 @@ public class AppTest {
     public void test1() {
         // your test goes here
         // A a = A.create("1");
+        CraftType craftType = CraftType.create(1);
 
     }
 
