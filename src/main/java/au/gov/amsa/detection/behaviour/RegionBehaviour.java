7ef0158c5415cc9b769efe4031b831a82431b2d0
Dave Moten <davidmoten@gmail.com>
2015-08-26 12:42:00 +1000

    one and any should return Optional

--- src/main/java/au/gov/amsa/detection/behaviour/RegionBehaviour.java
+++ src/main/java/au/gov/amsa/detection/behaviour/RegionBehaviour.java
@@ -1,7 +1,8 @@
 package au.gov.amsa.detection.behaviour;
 
 import java.util.Date;
-import java.util.List;
+
+import com.google.common.base.Optional;
 
 import au.gov.amsa.detection.ArbitraryId;
 import au.gov.amsa.detection.model.Context;
@@ -28,46 +29,48 @@ public class RegionBehaviour implements Behaviour {
 
     @Override
     public void onEntryHasPosition(Position event) {
-        List<RegionCraft> list = RegionCraft.select(RegionCraft.Attribute.craft_R15_id
+        Optional<RegionCraft> rc = RegionCraft.select(RegionCraft.Attribute.craft_R15_id
                 .eq(event.getCraftID()).and(RegionCraft.Attribute.region_R5_id.eq(self.getId())))
-                .many();
+                .one();
         boolean inside = contains(event.getLatitude(), event.getLongitude());
-        RegionCraft rc;
-        if (list.size() > 0) {
-            rc = list.get(0);
+        if (rc.isPresent()) {
             if (inside) {
-                rc.signal(RegionCraft.Events.In.builder().altitudeMetres(event.getAltitudeMetres())
-                        .latitude(event.getLatitude()).longitude(event.getLongitude())
-                        .time(event.getTime()).build());
+                rc.get().signal(RegionCraft.Events.In.builder()
+                        .altitudeMetres(event.getAltitudeMetres()).latitude(event.getLatitude())
+                        .longitude(event.getLongitude()).time(event.getTime()).build());
             } else {
-                rc.signal(RegionCraft.Events.Out.builder().altitudeMetres(event.getAltitudeMetres())
-                        .latitude(event.getLatitude()).longitude(event.getLongitude())
-                        .time(event.getTime()).build());
+                rc.get().signal(RegionCraft.Events.Out.builder()
+                        .altitudeMetres(event.getAltitudeMetres()).latitude(event.getLatitude())
+                        .longitude(event.getLongitude()).time(event.getTime()).build());
             }
         } else {
-            // Creation of RegionCraft that also sets its new state without
-            // triggering state machine
-            rc = RegionCraft.create(ArbitraryId.next());
-            if (inside) {
-                rc.setLastExitTimeFromRegion(new Date(Long.MIN_VALUE));
-                rc.setLastTimeEntered(event.getTime());
-                rc.setState(RegionCraft.State.INSIDE_REGION.toString());
-                self.getDetectionRule_R1()
-                        .forEach(dr -> dr.signal(DetectionRule.Events.PositionInRegion.builder()
-                                .altitudeMetres(event.getAltitudeMetres())
-                                .craftID(event.getCraftID()).latitude(event.getLatitude())
-                                .longitude(event.getLongitude()).time(event.getTime()).build()));
-            } else {
-                rc.setLastTimeEntered(event.getTime());
-                rc.setLastExitTimeFromRegion(new Date(Long.MAX_VALUE));
-                rc.setState(RegionCraft.State.OUTSIDE_REGION.toString());
-            }
-            Craft craft = Craft.find(event.getCraftID()).get();
-            rc.setRegion_R5(self);
-            rc.setCraft_R15(craft);
-            rc.persist(Context.em());
+            createNewRegionCraft(event, inside);
         }
 
+    }
+
+    private void createNewRegionCraft(Position event, boolean inside) {
+        // Creation of RegionCraft that also sets its new state without
+        // triggering state machine
+        RegionCraft rc = RegionCraft.create(ArbitraryId.next());
+        if (inside) {
+            rc.setLastExitTimeFromRegion(new Date(Long.MIN_VALUE));
+            rc.setLastTimeEntered(event.getTime());
+            rc.setState(RegionCraft.State.INSIDE_REGION.toString());
+            self.getDetectionRule_R1()
+                    .forEach(dr -> dr.signal(DetectionRule.Events.PositionInRegion.builder()
+                            .altitudeMetres(event.getAltitudeMetres()).craftID(event.getCraftID())
+                            .latitude(event.getLatitude()).longitude(event.getLongitude())
+                            .time(event.getTime()).build()));
+        } else {
+            rc.setLastTimeEntered(event.getTime());
+            rc.setLastExitTimeFromRegion(new Date(Long.MAX_VALUE));
+            rc.setState(RegionCraft.State.OUTSIDE_REGION.toString());
+        }
+        Craft craft = Craft.find(event.getCraftID()).get();
+        rc.setRegion_R5(self);
+        rc.setCraft_R15(craft);
+        rc.persist(Context.em());
     }
 
     private boolean contains(Double latitude, Double longitude) {
