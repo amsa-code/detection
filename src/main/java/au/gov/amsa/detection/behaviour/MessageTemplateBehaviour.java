4ef5bd00df6c0c9bc0a6358a97e058d652312908
Dave Moten <davidmoten@gmail.com>
2015-08-26 13:40:39 +1000

    dev

---
+++ src/main/java/au/gov/amsa/detection/behaviour/MessageTemplateBehaviour.java
@@ -0,0 +1,28 @@
+package au.gov.amsa.detection.behaviour;
+
+import au.gov.amsa.detection.ArbitraryId;
+import au.gov.amsa.detection.model.DetectionRule;
+import au.gov.amsa.detection.model.MessageTemplate;
+import au.gov.amsa.detection.model.MessageTemplate.Behaviour;
+import au.gov.amsa.detection.model.MessageTemplate.Events.Create;
+
+public class MessageTemplateBehaviour implements Behaviour {
+
+    private final MessageTemplate self;
+
+    public MessageTemplateBehaviour(MessageTemplate self) {
+        this.self = self;
+    }
+
+    @Override
+    public void onEntryCreated(Create event) {
+        self.setId(ArbitraryId.next());
+        self.setBody(event.getBody());
+        self.setSubject(event.getSubject());
+        self.setStartTime(event.getStartTime());
+        self.setEndTime(event.getEndTime());
+        self.setForceUpdateBeforeTime(event.getForceUpdateBeforeTime());
+        DetectionRule.find(event.getDetectionRuleID()).get().setMessageTemplate_R8(self);
+    }
+
+}
