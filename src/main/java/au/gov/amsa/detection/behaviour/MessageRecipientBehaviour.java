19d8b4388151dd9ce622eaaa594fad7d78f95913
Dave Moten <davidmoten@gmail.com>
2015-08-26 20:24:00 +1000

    dev

---
+++ src/main/java/au/gov/amsa/detection/behaviour/MessageRecipientBehaviour.java
@@ -0,0 +1,30 @@
+package au.gov.amsa.detection.behaviour;
+
+import au.gov.amsa.detection.model.DetectedCraft;
+import au.gov.amsa.detection.model.MessageRecipient;
+import au.gov.amsa.detection.model.MessageRecipient.Behaviour;
+import au.gov.amsa.detection.model.MessageRecipient.Events.Create;
+import au.gov.amsa.detection.model.MessageRecipient.Events.Send;
+
+public class MessageRecipientBehaviour implements Behaviour {
+
+    private final MessageRecipient self;
+
+    public MessageRecipientBehaviour(MessageRecipient self) {
+        this.self = self;
+    }
+
+    @Override
+    public void onEntryCreated(Create event) {
+        throw new RuntimeException("unexpected");
+    }
+
+    @Override
+    public void onEntrySent(Send event) {
+        if (self.getDetectedCraft_R14() != null) {
+            self.getDetectedCraft_R14().signal(DetectedCraft.Events.Send.builder()
+                    .detectionMessageID(event.getDetectionMessageID()).build());
+        }
+    }
+
+}
