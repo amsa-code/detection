9a1cd7d2a8e30c7f19192aa226f9b294b01c496a
Dave Moten <davidmoten@gmail.com>
2015-08-27 06:45:09 +1000

    send to detected craft

--- src/main/java/au/gov/amsa/detection/behaviour/DetectedCraftBehaviour.java
+++ src/main/java/au/gov/amsa/detection/behaviour/DetectedCraftBehaviour.java
@@ -1,17 +1,24 @@
 package au.gov.amsa.detection.behaviour;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import au.gov.amsa.detection.ArbitraryId;
 import au.gov.amsa.detection.model.Context;
+import au.gov.amsa.detection.model.Craft;
 import au.gov.amsa.detection.model.DetectedCraft;
 import au.gov.amsa.detection.model.DetectedCraft.Behaviour;
 import au.gov.amsa.detection.model.DetectedCraft.Events.Create;
 import au.gov.amsa.detection.model.DetectedCraft.Events.Send;
+import au.gov.amsa.detection.model.DetectionMessage;
 import au.gov.amsa.detection.model.DetectionRule;
 import au.gov.amsa.detection.model.MessageRecipient;
 
 public class DetectedCraftBehaviour implements Behaviour {
 
     private final DetectedCraft self;
+
+    private static final Logger log = LoggerFactory.getLogger(DetectedCraftBehaviour.class);
 
     public DetectedCraftBehaviour(DetectedCraft self) {
         this.self = self;
@@ -29,7 +36,10 @@ public class DetectedCraftBehaviour implements Behaviour {
 
     @Override
     public void onEntrySent(Send event) {
-        System.out.println("SENDING to DETECTED CRAFT");
+        DetectionMessage m = DetectionMessage.find(event.getDetectionMessageID()).get();
+        Craft craft = m.getDetection_R11().getCraft_R6();
+        log.info(String.format("SENDING to DETECTED CRAFT %s %s", craft.getIdentifierType(),
+                craft.getIdentifier()));
     }
 
 }
