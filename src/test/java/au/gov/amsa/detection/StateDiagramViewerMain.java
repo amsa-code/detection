5859e6755b58b9df8f549fac993d3fccbb97332e
Dave Moten <davidmoten@gmail.com>
2015-09-15 17:02:24 +1000

    add state diagram

--- src/test/java/au/gov/amsa/detection/StateDiagramViewerMain.java
+++ src/test/java/au/gov/amsa/detection/StateDiagramViewerMain.java
@@ -1,22 +1,12 @@
 package au.gov.amsa.detection;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import com.github.davidmoten.xuml.StateDiagramViewer;
-
-import xuml.tools.model.compiler.Util;
 
 public class StateDiagramViewerMain {
 
     public static void main(String[] args) throws IOException {
-        // StateDiagramViewer.show(createTestGraph());
-
-        try (InputStream is = StateDiagramViewerMain.class.getResourceAsStream("/domains.xml")) {
-            Util.getClasses(Util.getModeledDomain(is, "detection-domain")).stream().forEach(c -> {
-                System.out.println(c.getName());
-                StateDiagramViewer.show(c);
-            });
-        }
+        new StateDiagramViewer().start("/domains.xml", "detection-domain");
     }
 }
